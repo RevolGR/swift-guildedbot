79213bed936b132a6630cb9c612af28d4f8c097f
RevolGR <tetonick12@hotmail.com>
2022-08-05 20:17:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
+/*app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
-});
+});*/
 app.use(express.static('.'));
 app.listen(PORT, () =>{
     console.log(`App up at port ${PORT}`);
